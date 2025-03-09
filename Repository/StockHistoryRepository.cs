31895e447a4d6af7056455275da47ef292d4676a
Johnn-Aliferis <johnnaliferis@gmail.com>
2025-03-09 19:33:00 +0200

    Improve comment for performance for stock history retrieval.

--- Repository/StockHistoryRepository.cs
+++ Repository/StockHistoryRepository.cs
@@ -12,6 +12,11 @@ public class StockHistoryRepository(AppDbContext context) : IStockHistoryReposit
     {
        return await _stockHistories.Where(sh => sh.Stock.Symbol == symbol).ToListAsync();
     }
-    // For performance reasons , we can include pagination , and filtering based on start Date and end Date.
     // For simplicity reasons for this DEMO , we leave with fetching all history from DB.
+    
+    // Performance Optimization Note: 
+    // We can introduce the use of MATERIALIZED VIEWS during the insertion of the histories and update them periodically 
+    // to have precomputed data which will help with performance. 
+    // Another solution would be to use partitioning of the datatable , with some filtering with start / end date to get 
+    // only the data user needs , thus reducing costs.
 }
