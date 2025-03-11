a46bb5c912de4216470e755f1e5b0ff34120208b
Johnn-Aliferis <johnnaliferis@gmail.com>
2025-03-11 18:36:18 +0200

    Boilerplate code for portfolio controller. Next commit shall include : IPortfolioService and Implementation IPortfolioRepository and Implementation IPortfolioValidator and Implementation PortfoliosController code implementation for exposed RESTful API(create/delete portfolio).
    THEN remaining  :

---
+++ Controllers/PortfoliosController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockTradingApplication.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PortfoliosController: ControllerBase
+{
+    // Todo : Create two methods , one for creating a portfolio and one for deleting it.
+    //      add also the appropriate validations , unique per user , etc 
+}
