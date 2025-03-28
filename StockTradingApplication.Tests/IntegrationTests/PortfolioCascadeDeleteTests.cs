cd42e0109d7e29df8ec9f462e608e1ab6633486e
giannis-aliferis <giannis.aliferis@vodafone.com>
2025-03-28 16:40:02 +0200

    Added intergration tests via TestContainers for test for on delete cascading check once a portfolio exists and is being deleted.

---
+++ StockTradingApplication.Tests/IntegrationTests/PortfolioCascadeDeleteTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using StockTradingApplication.Common;
+using StockTradingApplication.Entities;
+using StockTradingApplication.Persistence;
+
+namespace StockTradingApplication.Tests.IntegrationTests;
+
+public class PortfolioCascadeDeleteTests(TestContainers containers) : IClassFixture<TestContainers>
+{
+    
+    private readonly AppDbContext _dbContext = containers.DbContext;
+
+    [Fact]
+    public async Task DeletingPortfolio_ShouldCascadeDelete_Holdings()
+    {
+        var stocks = _dbContext.Set<Stock>();
+        var users = _dbContext.Set<AppUser>();
+        var portfolios = _dbContext.Set<Portfolio>();
+        var portfoliosHoldings = _dbContext.Set<PortfolioHolding>();
+        var portfoliosTransactions = _dbContext.Set<PortfolioTransaction>();
+
+        var user = new AppUser { Id = 1, Username = "Test-Username"};
+        var stock = new Stock { Id = 1, Symbol = "Test-Symbol" , Currency = "Test-Currency" , Name = "Test-Name" , Price = 400m, CreatedAt = DateTime.UtcNow };
+        var portfolio = new Portfolio { Id = 1, CashBalance = 1000m, UserId = 1};
+        var holding = new PortfolioHolding { Id = 1, PortfolioId = 1, StockId = 1, Quantity = 5 };
+        var transaction = new PortfolioTransaction
+        {
+            Id = 1, PortfolioId = 1, StockId = 1, Quantity = 5, TransactionType = TransactionTypeEnum.Buy.ToString()
+        };
+
+        users.Add(user);
+        stocks.Add(stock);
+        portfolios.Add(portfolio);
+        portfoliosHoldings.Add(holding);
+        portfoliosTransactions.Add(transaction);
+
+        await _dbContext.SaveChangesAsync();
+
+        portfolios.Remove(portfolio);
+        await _dbContext.SaveChangesAsync();
+
+        // Portfolio Holdings should also be deleted
+        var holdingsAfterDelete = await portfoliosHoldings.ToListAsync();
+        holdingsAfterDelete.Should().BeEmpty();
+
+        // Portfolio Transactions should also be deleted
+        var transactionsAfterDelete = await portfoliosTransactions.ToListAsync();
+        transactionsAfterDelete.Should().BeEmpty();
+    }
+}
---
+++ StockTradingApplication.Tests/IntegrationTests/TestContainers.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using StockTradingApplication.Persistence;
+using Testcontainers.PostgreSql;
+
+namespace StockTradingApplication.Tests.IntegrationTests;
+
+public class TestContainers : IAsyncLifetime
+{
+    public required AppDbContext DbContext { get; set; }
+    private readonly PostgreSqlContainer _dbContainer;
+
+    public TestContainers()
+    {
+        _dbContainer = new PostgreSqlBuilder()
+            .WithDatabase("testdb")
+            .WithUsername("postgres")
+            .WithPassword("postgres")
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        var services = new ServiceCollection();
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(_dbContainer.GetConnectionString()));
+
+        var serviceProvider = services.BuildServiceProvider();
+        DbContext = serviceProvider.GetRequiredService<AppDbContext>();
+        
+        await DbContext.Database.EnsureCreatedAsync();
+        
+        // await DbContext.Database.MigrateAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _dbContainer.StopAsync();
+    }
+}
